a12b841713363caf745f158395f06eef40037341
asankul <asankul1993@gmail.com>
2022-08-13 18:36:13 +0300

    Initial Commit

---
+++ 7Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7Timer
+//
+//  Created by Асанкул Садыков on 13/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
