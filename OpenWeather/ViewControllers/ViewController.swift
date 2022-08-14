298816b1159d20c10e8897322eed79617b58f6c6
asankul <asankul1993@gmail.com>
2022-08-14 14:48:38 +0300

    Search bar added

--- OpenWeather/ViewControllers/ViewController.swift
+++ OpenWeather/ViewControllers/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var searchCity: UISearchBar!
     @IBOutlet var cityName: UILabel!
     @IBOutlet var weatherIcon: UIImageView!
     @IBOutlet var weatherTemp: UILabel!
@@ -18,17 +19,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         stackViewInfo.isHidden = true
-        activityIndicator.startAnimating()
-        activityIndicator.hidesWhenStopped = true
         fetchWeather()
+        searchCity.setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+        searchCity.searchTextField.textColor = .white
+        searchCity.delegate = self
     }
     
     private func setData(with forecast: Forecast) {
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
         cityName.text = forecast.name
         weatherTemp.text = "\(Int(forecast.main.temp))°C"
     }
+    
+    private func failedAlert() {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(
+                title: "Failed",
+                message: "We coudn't find the city, try again",
+                preferredStyle: .alert
+            )
+            
+            let okAction = UIAlertAction(title: "OK", style: .default)
+            alert.addAction(okAction)
+            self.present(alert, animated: true)
+            self.searchCity.text = ""
+        }
+    }
 
 
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let searchUrl = searchCity.text?.replacingOccurrences(of: " ", with: "%20")
+        fetchCityWeather(from: "https://api.openweathermap.org/data/2.5/weather?q=\(searchUrl ?? "limassol")&appid=12816ee3690f4ea2c47e71a8e2ea7412&units=metric")
+    }
 }
 
 // MARK: - Networking
@@ -41,6 +67,21 @@ extension ViewController {
                 self?.fetchImage(from: "https://openweathermap.org/img/wn/\(forecast.weather.first?.icon ?? "10g")@2x.png")
             case .failure(let error):
                 print(error)
+                self?.failedAlert()
+            }
+        }
+    }
+    
+    private func fetchCityWeather(from url: String) {
+        NetworkManager.shared.fetch(Forecast.self, from: url) { [weak self] result in
+            switch result {
+            case .success(let forecast):
+                self?.setData(with: forecast)
+                self?.fetchImage(from: "https://openweathermap.org/img/wn/\(forecast.weather.first?.icon ?? "10g")@2x.png")
+            case .failure(let error):
+                print(error)
+                print(url)
+                self?.failedAlert()
             }
         }
     }
